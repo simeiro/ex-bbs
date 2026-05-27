c0ade2695d0af6fe8c7fccec39b79fed39e3879b
simeiro <shitimi1633@gmail.com>
2026-05-27 18:01:26 +0900

    feat: 記事のみを投稿できるに画面を作成

--- src/main/java/com/example/ex_bbs/controller/ArticleController.java
+++ src/main/java/com/example/ex_bbs/controller/ArticleController.java
@@ -1,5 +1,11 @@
 package com.example.ex_bbs.controller;
 
+import com.example.ex_bbs.domain.Article;
+import com.example.ex_bbs.form.ArticleForm;
+import com.example.ex_bbs.repository.ArticleRepository;
+import jakarta.servlet.ServletContext;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,12 +16,17 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class ArticleController {
 
+    @Autowired
+    private ServletContext application;
+    @Autowired
+    private ArticleRepository articleRepository;
+
     /**
      *
      * @return
      */
     @GetMapping("")
-    public String index() {
+    public String index(ArticleForm articleForm) {
         return "article";
     }
 
@@ -24,7 +35,12 @@ public class ArticleController {
      * @return
      */
     @PostMapping("insert-article")
-    public String insertArticle() {
+    public String insertArticle(ArticleForm articleForm) {
+        Article article = new Article();
+        BeanUtils.copyProperties(articleForm, article);
+        articleRepository.insert(article);
+        application.setAttribute("articles", articleRepository.findAll());
+
         return "article";
     }
 
